d1f3b08be247cf6d1c73796522198e956a9d32e8
opedrohra <pedrorabeo0202@gmail.com>
2026-04-08 21:04:49 -0300

    feat: conclui pratica03. Fecha #ID

---
+++ praticas/pratica03/tipos_especiais.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+
+int main(){
+    int pais;
+    int idiomas;
+    float populacao;
+    long double aurea;
+    aurea = 1.61803398874989484820L;
+    pais = 195;
+    idiomas = 7100;
+    populacao = 8274065924;
+    printf("%i\n", pais);
+    printf("%i\n", idiomas);
+    printf("%.0f\n", populacao);
+    printf("%.20Lf\n", aurea);
+
+
+
+    return 0;
+}
